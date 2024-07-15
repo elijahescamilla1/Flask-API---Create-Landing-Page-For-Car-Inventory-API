194aaec8ee6b0848d63e374e2ac889650a5e5a64
Elijah Escamillagit <elijah.escamilla@gmail.com>
2024-07-15 13:17:37 -0700

    Changes

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+#Just for testing
